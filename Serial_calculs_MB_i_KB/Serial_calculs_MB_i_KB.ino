a5c5dc226aa6658c67cbd5b2c5d5cdb8d86480da
rogermurt11 <rogermurt@hotmail.com>
2016-03-06 20:09:59 +0100

    Serial calculs MB i KB
    gf

--- Serial_calculs_MB_i_KB/Serial_calculs_MB_i_KB.ino
+++ Serial_calculs_MB_i_KB/Serial_calculs_MB_i_KB.ino
@@ -1,3 +1,4 @@
+
 int drive_gb = 100;
 long drive_mb;
 long drive_kb;
@@ -11,15 +12,58 @@ void setup()               //run once, when the sketch starts
   Serial.print ("Your HD is ");
   Serial.print (drive_gb);
   Serial.println (" GB large");
-  Serial.println ("In theory, it can store ");
+  Serial.print ("In theory, it can store ");
+  
   drive_mb = drive_gb;
-  drive_mb = drive_gb/100000 ;
+  drive_mb = drive_mb * 1024;
+  
   Serial.print (drive_mb);
-  Serial.print ("Megabytes,"); 
+  Serial.print (" Megabytes,"); 
 
+  drive_kb = drive_gb;
+  drive_kb = drive_kb * 1024000;
+
+  Serial.print (drive_kb);
+  Serial.println (" Kilobytes.");
+
+  Serial.print ("But it really only stores ");
+
+  real_drive_mb = drive_gb;
+  real_drive_mb = real_drive_mb * 1000;
+
+  Serial.print (real_drive_mb);
+  Serial.print (" Megabytes,");
+
+  real_drive_kb = drive_gb;
+  real_drive_kb = real_drive_kb * 1000000;
+
+  Serial.print ( real_drive_kb);
+  Serial.println (" Kilobytes.");
+
+  Serial.print ("You are missing ");
+
+
+  Serial.print ( "Kilobytes!");
+
+  
+  
 }
 
 void loop()      //we need this to be here even though its empty
 {
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

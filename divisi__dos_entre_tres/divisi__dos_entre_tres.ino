2f91e5d02dda1ae80f60d6a0a37da0398ac6211f
rogermurt11 <rogermurt@hotmail.com>
2016-03-08 20:10:43 +0100

    divisio dos entre tres
    divisio decimal

---
+++ divisi__dos_entre_tres/divisi__dos_entre_tres.ino
@@ -0,0 +1,17 @@
+int a = 3.0;
+int b = 2.0;
+float c;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin (9600);
+
+Serial.println ( " tres entre dos es igual a ");
+
+Serial.print (" a / b = ");
+Serial.println ( (float) a / b ); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

70e3d08b5933e5ad5c37e3b19460f4fd7f0b8b6c
rogermurt11 <rogermurt@hotmail.com>
2016-03-11 20:30:20 +0100

    control estructures 3

--- control_estructures_2/control_estructures_2.ino
+++ control_estructures_2/control_estructures_2.ino
@@ -1,20 +1,22 @@
 int tempAigua = 100;
 
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
 Serial.begin (9600); 
 
-if (tempAigua < 100 );
+if(tempAigua < 100)
 {
 Serial.print ("Aigua encara no vull"); 
 }
-if else  (tempAigua >= 100);
+if  (tempAigua >= 100)
 {
 Serial.print ("Aigua bullint"); 
 
 }
-
-void loop() {
+}
+void loop() 
+{
   // put your main code here, to run repeatedly:
 
 }
---
+++ control_estructures_3/control_estructures_3.ino
@@ -0,0 +1,29 @@
+int tempAigua = 95;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+Serial.begin (9600); 
+
+if ((tempAigua < 100) (tempAigua!(tempAigua  >90) && (tempAigua <100)))
+{
+Serial.print ("Aigua encara no vull"); 
+}
+if ((tempAigua  >90) && (tempAigua <100))
+{
+Serial.print ("Aigua a punt de bullir");
+}
+if  (tempAigua = 100)
+{
+Serial.print ("Aigua a 100Cº"); 
+}
+if (tempAigua >100)
+{
+Serial.print ("Aigua bullint");
+}
+}
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+}

fd99d92932d15f85567df83504e3f6b3bf667e1f
rogermurt11 <rogermurt@hotmail.com>
2016-03-18 20:31:24 +0100

    if else i for

--- control_estructures_3/control_estructures_3.ino
+++ control_estructures_3/control_estructures_3.ino
@@ -1,3 +1,20 @@
+
+
+/*************************************************************************
+**                                                                      **                                                                     
+**                    Engegada aturada d'un LED                         **                                                                      
+**                                                                      **                                                                      
+**                                                                      **
+*************************************************************************/
+
+//******  Includes  ******************************************************
+
+
+//******  Variables  *****************************************************
+
+
+//******  Setup  *********************************************************                                                                      
+
 int tempAigua = 95;
 
 void setup() 
@@ -5,25 +22,27 @@ void setup()
   // put your setup code here, to run once:
 Serial.begin (9600); 
 
-if ((tempAigua < 100) (tempAigua!(tempAigua  >90) && (tempAigua <100)))
+if  (tempAigua  <90) 
 {
 Serial.print ("Aigua encara no vull"); 
 }
-if ((tempAigua  >90) && (tempAigua <100))
+ else if ((tempAigua >=90) && (tempAigua <100))
 {
 Serial.print ("Aigua a punt de bullir");
 }
-if  (tempAigua = 100)
+else if  (tempAigua == 100)
 {
 Serial.print ("Aigua a 100Cº"); 
 }
-if (tempAigua >100)
+else if (tempAigua >100)
 {
 Serial.print ("Aigua bullint");
 }
 }
+
+
+//******  Loop  **********************************************************
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-
+ 
 }
